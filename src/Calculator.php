495dc81c3869863acfeba8ecc367dae7a6f6dbe4
jesse.vandenkerckhove <jesse.vandenkerckhove@student.howest.be>
2021-07-01 04:50:37 +0200

    Square root functionality with simple Binary Search has been added

--- src/Calculator.php
+++ src/Calculator.php
@@ -40,8 +40,43 @@ class Calculator
         return 1/self::PositivePower($ground, -$exp);
     }
 
-    public static function SquareRoot(int $x) {
-        return $x;
+    public static function SquareRoot(float $x):float { //BINARY SEARCH FOR SQUARE ROOT
+                                                        // It works with two loops, the first loops try's to get close to full integers (no floats).
+                                                        // The second tries to get closer behind the 0 if it is necessary. The precision is decided by the precision variable.
+                                                        // This binary search tries to get closer to the answer and once it is close enough it gives the result.
+        $start = 0;
+        $mid = 0;
+        $end = $x;
+        $precision = 5;
+        $result = null;
+        while ($start <= $end) {
+            $mid = ($start + $end)/2;
+            if($mid * $mid == $x) {
+                $result = $mid;
+                return $result;
+            }
+            if($mid * $mid < $x) {
+                $start = $mid + 1;
+                $result = $mid;
+            }
+            else {
+                $end = $mid -1;
+            }
+        }
+        $increment = 0.1;
+        for ($i = 0; $i < $precision; $i++)
+        {
+            while ($result * $result <= $x)
+            {
+                $result += $increment;
+            }
+
+            // loop terminates when
+            // ans * ans > number
+            $result = $result - $increment;
+            $increment = $increment / 10;
+        }
+        return $result;
     }
 
 
